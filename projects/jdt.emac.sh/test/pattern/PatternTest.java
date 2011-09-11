e2eee11f0269b64d348ea0d4a03f49817a897e32
Emac <shen.bin.1983@gmail.com>
2011-09-11 11:17:46 +0800

    add test case of pattern BOL/EOL

--- projects/jdt.emac.sh/test/pattern/PatternTest.java
+++ projects/jdt.emac.sh/test/pattern/PatternTest.java
@@ -19,16 +19,31 @@ public class PatternTest
     private static final Pattern PATTERN_EBOX_RELEASE_LABEL = Pattern
                                                                     .compile("(EBOX_(S14_)?)(\\d{1,})_(Q[1-4])(.\\d{1,})?");
 
+    private static final Pattern PATTERN_BEOL               = Pattern.compile("^\\.(jpg|jpeg|png|gif)$");
+
     @Test
     public void testGroup()
     {
         Matcher matcher = PATTERN_EBOX_RELEASE_LABEL.matcher("EBOX_2010_Q2.1");
         Assert.assertTrue(matcher.matches());
         Assert.assertTrue(".1".equals(matcher.group(5)));
-        
+
         matcher = PATTERN_EBOX_RELEASE_LABEL.matcher("EBOX_2010_Q2");
         Assert.assertTrue(matcher.matches());
         Assert.assertNull(matcher.group(5));
+    }
+
+    @Test
+    public void testBEOL()
+    {
+        Matcher matcher = PATTERN_BEOL.matcher(".jpg");
+        Assert.assertTrue(matcher.matches());
+
+        matcher = PATTERN_BEOL.matcher("1.jpg");
+        Assert.assertFalse(matcher.matches());
+
+        matcher = PATTERN_BEOL.matcher(".jpg ");
+        Assert.assertFalse(matcher.matches());
     }
 
 }
